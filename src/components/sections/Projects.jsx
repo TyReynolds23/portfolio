4d588db42310eb45dd42f149d46284308c95f055
Ty Reynolds <tyreynolds.dev@gmail.com>
2025-02-27 19:54:29 -0600

    added portfolio project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -23,7 +23,25 @@ export const Projects = () => {
                             </div>
                             <div className="flex justify-evenly items-center">
                                 <a className="text-blue-400 hover:text-blue-300 transition-colors"> View Project →</a>
-                                <a href="https://github.com/TyReynolds23/crud-app"><img src="/portfolio/images/github-mark-white.png" alt="GitHub Logo" className="h-7 w-7"></img></a>
+                                <a href="https://github.com/TyReynolds23/crud-app"><img src="/images/github-mark-white.png" alt="GitHub Logo" className="h-7 w-7"></img></a>
+                            </div>
+                        </div>
+
+                        <div className="p-6 rounded-xl border border-white/10 hover:translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px+8px_rgba(59,130,246,0.2)] transition">
+                            <h3 className="text-xl font-bold mb-2">My Portfolio</h3>
+                            <p className="text-gray-400 mb-4">
+                                My portfolio website that you are on right now. Click the link to see the code.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {['TailwindCSS', 'React'].map((tech, key) => (
+                                    <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
+                                        {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex justify-evenly items-center">
+                                <a className="text-blue-400 hover:text-blue-300 transition-colors"> View Project →</a>
+                                <a href="https://github.com/TyReynolds23/portfolio"><img src="/images/github-mark-white.png" alt="GitHub Logo" className="h-7 w-7"></img></a>
                             </div>
                         </div>
                     </div>
